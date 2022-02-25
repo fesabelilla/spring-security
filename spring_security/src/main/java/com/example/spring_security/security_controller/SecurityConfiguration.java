83919342862a0e230d98613b1d510969a4050547
fesabelilla <fesabelilla@gmail.com>
2022-02-25 23:57:17 +0600

    Authorization using HttpSecurity

--- spring_security/src/main/java/com/example/spring_security/security_controller/SecurityConfiguration.java
+++ spring_security/src/main/java/com/example/spring_security/security_controller/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.spring_security.security_controller;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -10,22 +11,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    //Authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //super.configure(auth);
         //set configuration for Auth object
         auth.inMemoryAuthentication()
                 .withUser("zahid")
-                .password("12345678")
+                .password("1234")
                 .roles("USER")
                 .and()
                 .withUser("fesabelilla")
-                .password("12345678")
+                .password("1234")
                 .roles("ADMIN");
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    //Authorization
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin();
     }
 }
