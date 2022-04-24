6edd92f313aa73d46ec167b3a8f9acc7c4a573c6
fesabelilla <fesabelilla@gmail.com>
2022-04-24 20:55:24 +0600

    restore main

--- spring-security-jpa/src/main/java/com/example/springsecurityjpa/security/SecurityConfiguration.java
+++ spring-security-jpa/src/main/java/com/example/springsecurityjpa/security/SecurityConfiguration.java
@@ -1,15 +1,21 @@
 package com.example.springsecurityjpa.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
 
     //security jpa config
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+        auth.userDetailsService(userDetailsService);
     }
 
     @Override
