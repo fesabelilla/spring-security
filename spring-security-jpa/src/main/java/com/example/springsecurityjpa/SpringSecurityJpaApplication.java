87883877045b8fbf4c3926f8ac64ccb3347d7f27
fesabelilla <fesabelilla@gmail.com>
2022-05-08 23:42:08 +0600

    hard coded user details object security done

--- spring-security-jpa/src/main/java/com/example/springsecurityjpa/SpringSecurityJpaApplication.java
+++ spring-security-jpa/src/main/java/com/example/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -2,8 +2,9 @@ package com.example.springsecurityjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringSecurityJpaApplication {
 
 	public static void main(String[] args) {
