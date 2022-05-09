091f430d21ef71e423c8d79662d783fe260ed882
fesabelilla <fesabelilla@gmail.com>
2022-05-09 23:23:41 +0600

    Spring Security with JPA authentication and MySQL impl | have an issue

--- spring-security-jpa/src/main/java/com/example/springsecurityjpa/SpringSecurityJpaApplication.java
+++ spring-security-jpa/src/main/java/com/example/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -1,10 +1,13 @@
 package com.example.springsecurityjpa;
 
+import com.example.springsecurityjpa.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class SpringSecurityJpaApplication {
 
 	public static void main(String[] args) {
--- spring-security-jpa/src/main/java/com/example/springsecurityjpa/model/MyUserDetails.java
+++ spring-security-jpa/src/main/java/com/example/springsecurityjpa/model/MyUserDetails.java
@@ -6,26 +6,36 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyUserDetails implements UserDetails {
 
     private String username;
+    private String password;
+    private boolean active;
+    private List<GrantedAuthority> authorities;
+
+    public MyUserDetails(User user) {
+        this.username = user.getUserName();
+        this.password = user.getPassword();
+        this.active = user.isActive();
+        this.authorities = Arrays.stream(user.getRoles().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
 
     public MyUserDetails() {
     }
 
-    public MyUserDetails(String username) {
-        this.username = username;
-    }
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return "pass";
+        return password;
     }
 
     @Override
@@ -50,6 +60,6 @@ public class MyUserDetails implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return true;
+        return active;
     }
 }
