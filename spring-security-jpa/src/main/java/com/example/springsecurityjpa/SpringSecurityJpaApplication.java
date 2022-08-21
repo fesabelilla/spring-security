99723df3a65aabb42f9c236782f5bce89077bd6c
fesabelilla <fesabelilla@gmail.com>
2022-08-21 22:07:06 +0600

    spring-security-jwt init

--- spring-security-jpa/src/main/java/com/example/springsecurityjpa/SpringSecurityJpaApplication.java
+++ spring-security-jpa/src/main/java/com/example/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -3,7 +3,6 @@ package com.example.springsecurityjpa;
 import com.example.springsecurityjpa.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
--- spring-security-jpa/src/main/java/com/example/springsecurityjpa/model/User.java
+++ spring-security-jpa/src/main/java/com/example/springsecurityjpa/model/User.java
@@ -3,7 +3,7 @@ package com.example.springsecurityjpa.model;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "User")
+@Table(name = "user")
 public class User {
 
     @Id
